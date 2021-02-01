dd6fe29c8a0897d01851dd00a6e1ebbc3b241887
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-31 18:36:12 -0600

    edits to index and added user router to server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
+const userRouter = require("./users/users-router")
 
 const server = express()
 
 server.use(cors())
 server.use(helmet())
 server.use(express.json())
+
+server.use("/users", userRouter)
+
 
 server.get("/", (req, res) => {
 	res.json({
